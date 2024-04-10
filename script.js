e44e3e78dede5f4b7925e78ace4e7586f3c33dca
Aloefuna <93980249+ifymiracle@users.noreply.github.com>
2024-04-10 12:45:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-// creating button click show hide navbar
-var togglebtn= document.querySelector(".togglebtn");
-var nav= document.querySelector(".navlink");
-var link = document.querySelector(".navlink li");
+// // creating button click show hide navbar
+// // var togglebtn= document.querySelector(".togglebtn");
+// // var nav= document.querySelector(".navlink");
+// // // var link = document.querySelector(".navlink ");
 
-togglebtn.addEventListener("click", function(){
-    this.classList.toggle("click");
-    nav.classList.toggle("open" )
-})
+// // togglebtn.addEventListener("click", function(){
+// //     nav.classList.toggle("open" )
+// //     this.classList.toggle("click");
+   
+// })
+
+let togglebtn = document.querySelector(".togglebtn");
+let nav = document.querySelector(".navlink");
+
+
+togglebtn.onclick = function(){
+    togglebtn.classList.toggle("click");
+    nav.classList.toggle("open");
+}
+
+
+
+
+
+ document.onclick = function(e){
+    if (!togglebtn.contains(e.target) && !nav.contains(e.target) ) {
+        togglebtn.classList.remove("click");
+        nav.classList.remove("open");
+    }
+}
